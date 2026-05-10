cd1352c4432c010af273a5156477a79166f31f86
tryrodave <73838305+DaveVermeulen@users.noreply.github.com>
2026-05-10 09:57:49 +0300

    Neoforge EventBus deprecation fix

--- src/main/java/org/fynixx/dungbeetle/Dungbeetle.java
+++ src/main/java/org/fynixx/dungbeetle/Dungbeetle.java
@@ -123,7 +123,7 @@ public class Dungbeetle {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
